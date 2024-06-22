32675fd7c39a4e9cc6297b3901708448dba4c612
booleanadv97 <staff_badgame@yahoo.com>
2024-06-22 13:25:06 +0200

    Implementation of real-time traffic flow status request & search for nearby restaurants.

--- app.py
+++ app.py
@@ -2,15 +2,40 @@ from flask import Flask, request, jsonify
 import json
 import requests
 import googlemaps
+import random
 
 # Telegram bot token
 BOT_TOKEN = "7359117515:AAGgeMyoN7sQ0VbEMQI0Bnx9B48FMXVMzto"
+TOMTOM_API = "bug3YWud39DJDANkBAVsTn3DEuONsGRj"
 WEATHER_API_KEY = "a26d52cbb20d67a2adb5379a13c920a7"
 
 app = Flask(__name__)
 
 # Google maps configuration
 gmaps = googlemaps.Client(key="AIzaSyBCg-wRG41EjUesSn3Z6hEjPoZG3BbDQEE")
+
+def puglia_facts():
+    number = int(random.uniform(0, 7))
+    if number == 0:
+        return "Miracles in Lecce: In Piazza Sant\'Oronzo, the eighteenth-century statue of the Patron Saint sits on a bare column, originally from the town of Brindisi. This column marked the end of the Via Appia, one of the most important roads in Ancient Rome. It was donated by the people of Brindisi to the Patron Saint of Lecce, when the town was miraculously spared by the plague that ravaged Salento in the seventeenth century."
+    elif number == 1:
+        return "Swindlers in Alberobello: Famous for its trulli, one of the house styles you can only find in Puglia, this town was gifted to the first Count of Conversano by Robert of Anjou, as a reward for taking part in the Crusades. The oldest trulli date back to the fourteenth century and they were built without mortar so that they could be easily dismantled in case the emissaries of the Kingdom of Naples, which imposed heavy taxes on any new house, came by to check on the inhabitants."
+    elif number == 2:
+        return "Gods and dolphins in Taranto: According to Greek mythology, Taras was the son of the nymph Satyria and Neptune, the god of the sea. He was the head of a fleet that arrived on the banks of the river where the town was later built. He immediately started offering sacrifices to thank his father for the good voyage they had and to enquire whether it was wise to build a town here when suddenly he saw a dolphin jump into the waters of the river. The young man interpreted this apparition as a sign of Neptune\'s encouragement to found Taranto."
+    elif number == 3:
+        return "Along the Templar trail in Puglia: The history of the Knights Templar is certainly one of the most famous and fascinating in Christian history. Many people are familiar with it but few know that the Knights of Christ and the Temple of Solomon were also present in Puglia. The first official document attesting to the presence of the Templars in the region dates back to 1143."
+    elif number == 4:
+        return "Puglia is home to over 50 million olive trees: Many of them centuries old. Puglia needed to enact laws to deter people from other parts of Italy coming down and digging up those valuable olive trees to bring them back and plant them on their land. It\'s now illegal to dig up a tree from Puglian soil."
+    elif number == 5:
+        return "Puglia is famous for its olive oil production: The region provides around 40% of the country\'s olive oil, which amounts to around 300,000 tonnes every single year."
+    elif number == 6:
+        return "Puglia has the longest coastline of any Italian mainland region: The heel of Italy\'s boot-like shape is the defining geographical feature of the area, and is a key reason behind the flowing lengths of coast, totalling around 800km."
+    elif number == 7:
+        return "Puglia was originally colonised by Mycenaean Greeks: One of Italy\'s most archaeologically interesting areas, Puglia is an absolute hub for history."
+    else:
+        return "Puglia is pure magic."
+
+
 
 # Function to send messages via Telegram API
 def send_message(chat_id, text):
@@ -19,6 +44,29 @@ def send_message(chat_id, text):
   response = requests.post(url, json=payload)
   if response.status_code != 200:
     print(f"Error while sending the message: {response.text}")
+
+def get_coords(place):
+  coords = gmaps.geocode(place)
+  if coords:
+    lat = coords[0]["geometry"]["location"]["lat"]
+    lng = coords[0]["geometry"]["location"]["lng"]
+    return lat,lng
+  return None
+
+def get_traffic_flow(city):
+  lat,lng = get_coords(city)
+  if lat and lng:
+    url = f"https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?point={lat},{lng}&key={TOMTOM_API}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    current_speed = data['flowSegmentData']['currentSpeed']
+    free_flow_speed = data['flowSegmentData']['freeFlowSpeed']
+    if current_speed < free_flow_speed * 0.7:  
+        return "There is traffic congestion in the given location."
+    else:
+        return "Traffic is flowing smoothly in the given location."
+  else:
+    return "Could not retrieve location."
 
 def build_list_of_places(places):
   result = ""
@@ -49,12 +97,8 @@ def get_current_weather(api_key, city_name):
 
 def get_places(city_name, search_type, search_radius=5000):
   try:
-    locations = gmaps.geocode(city_name)
-    if locations:
-      # Extract latitude and longitude from the user's location
-      lat = locations[0]["geometry"]["location"]["lat"]
-      lng = locations[0]["geometry"]["location"]["lng"]
-
+    lat,lng = get_coords(city_name)
+    if lat and lng:
       # Build the attractions API request
       places = gmaps.places_nearby(
           location=(lat, lng), radius=search_radius, type=search_type
@@ -71,7 +115,7 @@ def get_places(city_name, search_type, search_radius=5000):
         result.append(place_data)
       return result
     else:
-      print(f"Error city not found: {city_name}")
+      print(f"Error location not found: {city_name}")
   except googlemaps.exceptions.HTTPError as err:
     print(f"Error during API request: {err}")
   return None
@@ -86,26 +130,36 @@ def process_message():
   if intent == "Start":
     response = "Hello, I am your personal assistant during your stay in Puglia. If you need help, don\'t hesitate to ask."
   elif intent == "Help":
-    response = "Hello, I can help you find the nearest hotel, recommend a place to enjoy typical Apulian dishes, suggest one of the magical spots in Puglia, or provide real-time updates on weather, traffic, and road conditions."
+    response = "Hello, I can help you find the nearest hotel, recommend a place to enjoy typical Apulian dishes, suggest one of the magical spots in Puglia, provide real-time updates on weather, traffic, and road conditions or simply make your day better with a trivia."
   elif intent == "RequestLodging":
     response = "Sure, please provide your current location so that I can create a list of accommodations closest to you."
-  # Request user location in order to find nearby attractions
+  elif intent == "RequestTrafficUpdate":
+    response = "Sure, please provide your current location so that I can inform you about current traffic status."
   elif intent == "RequestAttractions":
     response = "Sure, please provide your current location so that I can create a list of nearby places to visit."
+  elif intent == "RequestRestaurantsNearby":
+    response = "Sure, please provide your current location so that I can create a list of nearby restaurants where you can eat."
   elif intent == "RequestWeatherUpdate":
     response = "Sure, please provide your current location so that I can provide you real-time weather information."
-  # Providing a list of attractions nearby user's location
+  elif intent == "Trivia":
+    response = "Here's an interesting fact about Puglia: \n\n" + puglia_facts()
   elif intent == "UserProvidesLocation":
     location = data['queryResult']['parameters'].get('location', None)
+    response = str(location)
     if location:
       context = data['queryResult']['outputContexts'][0]['name']
       if "userrequestattractions" in context:
-        attractions = get_places(location['city'],"tourist_attraction")
-        response = build_list_of_places(attractions)
+        attractions = get_places(location['street-address'] +", "+ location['city'],"tourist_attraction")
+        response = "Here's a list of nearby attractions: \n\n" + build_list_of_places(attractions)
       elif "userrequestlodging" in context:
-        accomodations = get_places(location['city'], "lodging")
-        response = build_list_of_places(accomodations)
+        accomodations = get_places(location['street-address'] +", "+ location['city'], "lodging")
+        response = "Here's a list of nearby accomodations: \n\n" + build_list_of_places(accomodations)
+      elif "userrequestrestaurant" in context:
+        restaurants = get_places(location['street-address'] +", "+ location['city'], "restaurant")
+        response = "Here's a list of nearby restaurants: \n\n" + build_list_of_places(restaurants)
       elif "userrequestweatherupdate" in context:
         response = get_current_weather(WEATHER_API_KEY, location['city'])
+      elif "userrequesttrafficupdate" in context:
+        response = get_traffic_flow(location['street-address'] +", "+ location['city'])
   send_message(chat_id, response)
   return jsonify({"fulfillmentText": response})   
