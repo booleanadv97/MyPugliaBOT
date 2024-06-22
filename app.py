9f5021c2a9f679654c54d884156897a418770274
booleanadv97 <staff_badgame@yahoo.com>
2024-06-22 16:55:32 +0200

    Multilingual support (en-us/it)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import googlemaps
 import random
 
-# Telegram bot token
+# API keys
 BOT_TOKEN = "7359117515:AAGgeMyoN7sQ0VbEMQI0Bnx9B48FMXVMzto"
 TOMTOM_API = "bug3YWud39DJDANkBAVsTn3DEuONsGRj"
 WEATHER_API_KEY = "a26d52cbb20d67a2adb5379a13c920a7"
@@ -14,6 +14,7 @@ app = Flask(__name__)
 # Google maps configuration
 gmaps = googlemaps.Client(key="AIzaSyBCg-wRG41EjUesSn3Z6hEjPoZG3BbDQEE")
 
+# TO-DO multilingual support 
 def puglia_facts():
     number = int(random.uniform(0, 7))
     if number == 0:
@@ -67,7 +68,7 @@ def get_coords(place):
     return lat,lng
   return None
 
-def get_traffic_flow(city):
+def get_traffic_flow(city, language):
   lat,lng = get_coords(city)
   if lat and lng:
     url = f"https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?point={lat},{lng}&key={TOMTOM_API}"
@@ -76,19 +77,28 @@ def get_traffic_flow(city):
     current_speed = data['flowSegmentData']['currentSpeed']
     free_flow_speed = data['flowSegmentData']['freeFlowSpeed']
     if current_speed < free_flow_speed * 0.7:  
-        return "There is traffic congestion in the given location."
+        if language == "it":
+          return "C\'è congestione del traffico nella posizione fornita."
+        else:
+          return "There is traffic congestion in the given location."
     else:
-        return "Traffic is flowing smoothly in the given location."
+        if language == "it":
+          return "Il traffico scorre fluidamente nella posizione fornita."
+        else:
+          return "Traffic is flowing smoothly in the given location."
   else:
     return "Could not retrieve location."
 
-def build_list_of_places(places):
+def build_list_of_places(places, language):
   result = ""
   for place in places:
-    result += place["name"]+". Address: "+place["address"]+".\n\n"
+    if language == "it":
+      result += place["name"]+". Indirizzo: "+place["address"]+".\n\n"
+    else:
+      result += place["name"]+". Address: "+place["address"]+".\n\n" 
   return result
 
-def get_current_weather(api_key, city_name):
+def get_current_weather(api_key, city_name, language):
     base_url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": city_name,
@@ -103,7 +113,10 @@ def get_current_weather(api_key, city_name):
             weather_description = data["weather"][0]["description"]
             temperature = data["main"]["temp"]
             humidity = data["main"]["humidity"]
-            return f"Current weather in {city_name}: {weather_description}, Temperature: {temperature}°C, Humidity: {humidity}%"
+            if language == "it":
+              return f"Condizioni meteo attuali in {city_name}: {weather_description}, Temperatura: {temperature}°C, Umidità: {humidity}%"
+            else:
+              return f"Weather conditions in {city_name}: {weather_description}, Temperature: {temperature}°C, Humidity: {humidity}%"
         else:
             return "City not found."
     except Exception as e:
@@ -140,23 +153,48 @@ def process_message():
   chat_id = data.get("session", {}).split("/")[-1]
   # Extract intent
   intent = data["queryResult"]["intent"]["displayName"]
+  language = data.get('queryResult', {}).get('languageCode')
   # Perform actions based on the intent
   if intent == "Start":
-    response = "Hello, I am your personal assistant during your stay in Puglia. If you need help, don\'t hesitate to ask."
+    if language == "it":
+      response = "Ciao, sono il tuo assistente personale durante il tuo soggiorno in Puglia. Per iniziare prova a chiedere 'aiuto'."
+    else:
+      response = "Hello, I am your personal assistant during your stay in Puglia. To begin you can ask for 'help'."
   elif intent == "Help":
-    response = "Hello, I can help you find the nearest hotel, recommend a place to enjoy typical Apulian dishes, suggest one of the magical spots in Puglia, provide real-time updates on weather, traffic, and road conditions or simply make your day better with a trivia."
+    if language == "it":
+      response = "Ciao, posso aiutarti a trovare l\'hotel più vicino, consigliarti un posto dove gustare piatti tipici pugliesi, suggerirti uno dei luoghi magici in Puglia, fornirti aggiornamenti in tempo reale sul meteo, il traffico e le condizioni stradali o semplicemente rendere la tua giornata migliore con una curiosità."
+    else:
+      response = "Hello, I can help you find the nearest hotel, recommend a place to enjoy typical Apulian dishes, suggest one of the magical spots in Apulia, provide real-time updates on weather, traffic, and road conditions or simply make your day better with a trivia."
   elif intent == "RequestLodging":
-    response = "Sure, please provide your current location so that I can create a list of accommodations closest to you."
+    if language == "it":
+      response = "Certamente, per favore fornisci la tua posizione attuale in modo che io possa creare un elenco di alloggi più vicini a te."
+    else:
+      response = "Sure, please provide your current location so that I can create a list of accommodations closest to you."
   elif intent == "RequestTrafficUpdate":
-    response = "Sure, please provide your current location so that I can inform you about current traffic status."
+    if language == "it":
+      response = "Certamente, per favore fornisci la tua posizione attuale in modo che io possa informarti sullo stato attuale del flusso del traffico."
+    else:
+      response = "Sure, please provide your current location so that I can inform you about current traffic flow status."
   elif intent == "RequestAttractions":
-    response = "Sure, please provide your current location so that I can create a list of nearby places to visit."
+    if language == "it":
+      response = "Certamente, per favore fornisci la tua posizione attuale in modo che io possa creare un elenco di luoghi vicini da visitare."
+    else:
+      response = "Sure, please provide your current location so that I can create a list of nearby places to visit."
   elif intent == "RequestRestaurantsNearby":
-    response = "Sure, please provide your current location so that I can create a list of nearby restaurants where you can eat."
+    if language == "it":
+      response = "Certamente, per favore fornisci la tua posizione attuale in modo che io possa creare un elenco di ristoranti nelle vicinanze dove puoi mangiare."
+    else:
+      response = "Sure, please provide your current location so that I can create a list of nearby restaurants where you can eat."
   elif intent == "RequestWeatherUpdate":
-    response = "Sure, please provide your current location so that I can provide you real-time weather information."
+    if language == "it":
+      response = "Certamente, per favore fornisci la tua posizione attuale in modo che io possa fornirti informazioni meteo in tempo reale."
+    else:
+      response = "Sure, please provide your current location so that I can provide you real-time weather information."
   elif intent == "Trivia":
-    response = "Here's an interesting fact about Puglia: \n\n" + puglia_facts()
+    if language == "it":
+      response = "Ecco un fatto interessante sulla Puglia:"
+    else: 
+      response = "Here's an interesting fact about Puglia: \n\n" + puglia_facts()
   elif intent == "UserProvidesLocation":
     location = data['queryResult']['parameters'].get('location', None)
     response = str(location)
@@ -164,18 +202,21 @@ def process_message():
       context = data['queryResult']['outputContexts'][0]['name']
       if "userrequestattractions" in context:
           attractions = get_places(location['street-address'] +", "+ location['city'],"tourist_attraction")
-          response = "Here's a list of nearby attractions: \n\n" + build_list_of_places(attractions)
+          response = "Ecco una lista di attrazioni vicine: \n\n" + build_list_of_places(attractions, language)
       elif "userrequestlodging" in context:
         accomodations = get_places(location['street-address'] +", "+ location['city'], "lodging")
-        response = "Here's a list of nearby accomodations: \n\n" + build_list_of_places(accomodations)
+        response = "Ecco una lista di alloggi vicini: \n\n" + build_list_of_places(accomodations, language)
       elif "userrequestrestaurant" in context:
         restaurants = get_places(location['street-address'] +", "+ location['city'], "restaurant")
-        response = "Here's a list of nearby restaurants: \n\n" + build_list_of_places(restaurants)
+        response = "Ecco una lista di ristoranti in vicinanza: \n\n" + build_list_of_places(restaurants, language)
       elif "userrequestweatherupdate" in context:
-        response = get_current_weather(WEATHER_API_KEY, location['city'])
+        response = get_current_weather(WEATHER_API_KEY, location['city'], language)
       elif "userrequesttrafficupdate" in context:
-        response = get_traffic_flow(location['street-address'] +", "+ location['city'])
+        response = get_traffic_flow(location['street-address'] +", "+ location['city'], language)
     else:
-       response = "Invalid location, please provide a correct location within Apulia."
+      if language == "it":
+         response = "Posizione non valida, fornisci una posizione corretta all\'interno della Puglia."
+      else:
+        response = "Invalid location, please provide a correct location within Apulia."
   send_message(chat_id, response)
   return jsonify({"fulfillmentText": response})   
